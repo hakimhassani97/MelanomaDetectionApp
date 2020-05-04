3a4a02404ea63b690ace4dbadcf4b73d56563779
hakimhassani97 <hakimhassani97@gmail.com>
2020-05-04 18:57:41 +0100

    updated models

--- melanomaApp/models.py
+++ melanomaApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    '''
+        the User model
+    '''
+    username = models.CharField(max_length=20)
+    date = models.DateTimeField('registration date')
+
+    def __str__(self):
+        return self.username+' '+str(self.date)
