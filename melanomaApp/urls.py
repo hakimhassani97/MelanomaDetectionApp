f9d63c4600189fa4f1b10913cda6905b1c7e4d07
hakimhassani97 <hakimhassani97@gmail.com>
2020-05-05 19:23:23 +0100

    updated urls

--- melanomaApp/urls.py
+++ melanomaApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('forms.html', views.forms, name='forms'),
 ]
--- melanomaApp/views.py
+++ melanomaApp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .models import User
 
-def index(request):
+def forms(request):
     users = User.objects.order_by('-date')[:5]
     context = {
         'users': users,
     }
     return render(request, 'forms.html', context)
+
+def index(request):
+    users = User.objects.order_by('-date')[:5]
+    context = {
+        'users': users,
+    }
+    return render(request, 'index.html', context)
+
+def error_404(request, exception):
+    data = {}
+    return render(request,'404.html', data)
