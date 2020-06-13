4ce973da585be2b7c50cad5eab7b089f82002fff
hakimhassani97 <hakimhassani97@gmail.com>
2020-06-13 14:25:04 +0100

    fixed messages

--- melanomaApp/forms.py
+++ melanomaApp/forms.py
@@ -7,14 +7,14 @@ class LoginForm(forms.Form):
     '''
         Doctor login form
     '''
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Username","class": "form-control"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Password","class": "form-control"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Nom d'utilisateur","class": "form-control"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Mot de passe","class": "form-control"}))
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Username","class": "form-control"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Nom d'utilisateur","class": "form-control"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder" : "Email","class": "form-control"}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Password","class": "form-control"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Password check","class": "form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Mot de passe","class": "form-control"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Confirmation du mot de passe","class": "form-control"}))
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
@@ -23,19 +23,19 @@ class RegisterForm(forms.ModelForm):
     '''
         Doctor registration form
     '''
-    firstName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "First Name","class": "form-control","type":"text"}))
-    lastName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Last Name","class": "form-control","type":"text"}))
+    firstName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Prénom","class": "form-control","type":"text"}))
+    lastName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Nom","class": "form-control","type":"text"}))
     image = forms.ImageField(widget=forms.FileInput(attrs={"placeholder" : "Image","class": "form-control"}))
-    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Phone","class": "form-control"}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Téléphone","class": "form-control"}))
     class Meta:
         model = Doctor
         fields = ['phone', 'image','firstName','lastName']
 
 
 class ChangePassword(UserCreationForm) :
-    oldpassword =forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Old Password","class": "form-control"}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Password","class": "form-control"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Password check","class": "form-control"}))
+    oldpassword =forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Ancien mot de passe","class": "form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Mot de passe","class": "form-control"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder" : "Confirmation du mot de passe","class": "form-control"}))
     class Meta:
         model = User
         fields = ('oldpassword', 'password1', 'password2')
@@ -65,12 +65,12 @@ class AddPatientForm(forms.ModelForm):
     '''
         Add Patient form
     '''
-    firstName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "First Name","class": "form-control","type":"text"}))
-    lastName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Last Name","class": "form-control","type":"text"}))
+    firstName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Prénom","class": "form-control","type":"text"}))
+    lastName = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Nom","class": "form-control","type":"text"}))
     # email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder" : "Email","class": "form-control","email":"email" }))
-    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Phone","class": "form-control"}))
-    birthDate = forms.DateField(widget=forms.DateInput(format = '%Y-%m-%d', attrs={"placeholder" : "Birth Date","class": "form-control" ,"type" :"Date"}))
-    address = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Address","class": "form-control","type":"text"}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Téléphone","class": "form-control"}))
+    birthDate = forms.DateField(widget=forms.DateInput(format = '%Y-%m-%d', attrs={"placeholder" : "Date de naissance","class": "form-control" ,"type" :"Date"}))
+    address = forms.CharField(widget=forms.TextInput(attrs={"placeholder" : "Adresse","class": "form-control","type":"text"}))
     CHOICES = (('', 'Sexe'),('male', 'Male'),('femelle', 'Femelle'))
     sexe = forms.ChoiceField(choices=CHOICES ,widget=forms.Select(attrs={"placeholder" : "Sexe","class": "form-control"}))
     class Meta:
